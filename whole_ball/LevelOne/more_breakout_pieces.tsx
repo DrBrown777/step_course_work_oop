739849e5197b56b906d6987f7a45a9e9bd1d4c61
Dr_Brown <romanterziev@gmail.com>
2020-08-28 17:35:22 +0300

    v. 1.0

---
+++ whole_ball/LevelOne/more_breakout_pieces.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.1" name="more_breakout_pieces" tilewidth="16" tileheight="16" tilecount="4000" columns="80">
+ <image source="more_breakout_pieces.png" width="1280" height="800"/>
+</tileset>
